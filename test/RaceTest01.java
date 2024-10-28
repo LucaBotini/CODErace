6ae157615cc167615d20cfd76a68cb9b59ac0e44
Luca Botini <devbotini@gmail.com>
2024-10-28 00:41:11 -0300

    Ajuste de cálculo.
    apenas movi o "." de lugar. Estava com cálculo errado no valor de km

--- test/RaceTest01.java
+++ test/RaceTest01.java
@@ -4,7 +4,7 @@ import academy.devdojo.maratonajava.javacore.AATestesSolos.domain.Race;
 
 public class RaceTest01 {
     public static void main(String[] args) {
-        Race race = new Race("Laguna Seca",3.602);
+        Race race = new Race("Laguna Seca",3602.0);
         Car car = new Car("Honda","Civic SI", 2007, 224.8);
         Car car2 = new Car("AUDI","RS6 Avant", 2020, 283.7);
         Car car3 = new Car("Land Rover Range","Land Rover", 2013, 219.4);
